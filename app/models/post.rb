6f36f3e777f0f7de81d5b49c8fc35a316bdb1baf
= <alanivon8@gmail.com>
2016-04-30 19:15:24 +0800

    add simple fomat to show content

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-	validates_length_of :title, :minimum => 10
+	validates_length_of :title, :minimum => 5
 	validates :content, presence: true
 	validates :post_date, presence: {message: "have to be selected before you save!"}
 end
