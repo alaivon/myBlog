1ca4d9811ad0e2f702f91c771be9b66ca0a1db7a
= <alanivon8@gmail.com>
2016-05-03 13:49:13 +0800

    set sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,21 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+config.action_mailer.default_url_options = {host: 'https://billblog2016.herokuapp.com'} #Your App Domain 
+
+config.action_mailer.delivery_method = :smtp
+
+#Copy Code:
+
+  ActionMailer::Base.smtp_settings = {
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
+  :domain         => 'heroku.com',
+  :enable_starttls_auto => true
+}
   # Code is not reloaded between requests.
   config.cache_classes = true
 
