c48510b74ba56e314f201e7b5bc91c415ba026a1
= <alanivon8@gmail.com>
2016-05-03 14:24:29 +0800

    test2 sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-config.action_mailer.default_url_options = {host: 'https://billblog2016.herokuapp.com'} #Your App Domain 
+config.action_mailer.default_url_options = {host: 'http://billblog2016.herokuapp.com'} #Your App Domain 
 
 config.action_mailer.delivery_method = :smtp
 
